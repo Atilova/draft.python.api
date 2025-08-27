c88031babfb9e2514829bec6b62eabf6d2f4320c
atilova <ati2lova3@gmail.com>
2025-08-27 02:07:03 +0200

    feat: Setup CI workflow

--- src/tests/test_draft.py
+++ src/tests/test_draft.py
@@ -1,4 +1,14 @@
+import urllib.request
+
+
 def test_true_is_true():
     result = print("Ok") or True
+
+    with urllib.request.urlopen("http://nginx") as response:
+        status = response.status
+        data = response.read().decode("utf-8")
+
+    print("Status code ->", status)
+    print("Response body ->", data)
 
     assert result
